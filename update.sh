9ac9e75be748526e2759b8a5df71879a6578a355
Stoku <thestoku@gmail.com>
2023-11-24 17:54:11 +0100

    Update update.sh
    Fix previous incorrect commit.

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
-mkdir $PWD/tmp &&
-wget -nc https://gtaconnected.com/downloads/GTAC-Server-Linux-$gtacver.tar.gz /tmp &&
-tar -zxvf /tmp/GTAC-Server-Linux-$gtacver.tar.gz
+rm -drf tmp &&
+mkdir tmp &&
+cd tmp &&
+wget -nc https://gtaconnected.com/downloads/GTAC-Server-Linux-$gtacver.tar.gz &&
+tar -zxvf GTAC-Server-Linux-$gtacver.tar.gz &&
+rm -rf server.xml GTAC-Server-Linux-$gtacver.tar.gz &&
+cp -rf * ../ &&
+cd ../ &&
+rm -drf tmp
